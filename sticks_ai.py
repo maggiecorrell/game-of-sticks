614aea2bd93b4da4cd8649ca449b2b561ec2d24e
Maggie Correll <maggie.correll@gmail.com>
2016-07-20 08:21:37 -0400

    added file for AI

---
+++ sticks_ai.py
@@ -0,0 +1,5 @@
+import sticks
+
+class ArtificialIntelligence:
+
+    def __init__(self):
---
+++ sticks_test.py
@@ -0,0 +1,10 @@
+def test_choose_number_of_sticks():
+    number_of_sticks = 50
+
+    assert number_of_sticks == 50
+
+def test_take_away_count():
+    counter = 50
+    pick = 3
+
+    assert counter == 47
